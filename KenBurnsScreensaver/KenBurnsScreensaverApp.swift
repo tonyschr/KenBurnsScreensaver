5d36926ffd184e0f81398c3a1f294ee3ad3454d0
Tony Schreiner <tonyschr@users.noreply.github.com>
2026-07-21 09:54:33 -0600

    Add min/max ratings to settings

--- KenBurnsScreensaver/KenBurnsScreensaverApp.swift
+++ KenBurnsScreensaver/KenBurnsScreensaverApp.swift
@@ -45,9 +45,23 @@ final class AppSettings {
         }
     }
 
+    var minRating: Int {
+        didSet {
+            UserDefaults.standard.set(minRating, forKey: "minRating")
+        }
+    }
+
+    var maxRating: Int {
+        didSet {
+            UserDefaults.standard.set(maxRating, forKey: "maxRating")
+        }
+    }
+
     init() {
         slideDuration = UserDefaults.standard.double(forKey: "slideDuration")
         fadeDuration = UserDefaults.standard.double(forKey: "fadeDuration")
+        minRating = UserDefaults.standard.integer(forKey: "minRating")
+        maxRating = UserDefaults.standard.integer(forKey: "maxRating")
     }
 }
 
@@ -60,36 +74,66 @@ struct SettingsView: View {
     private var settings
     
     @State private var isEditing = false
+    @State private var selectedValue: Int? = nil
     
     var body: some View {
         Form {
-            VStack {
-                HStack {
-                    Slider(
-                        value: Bindable(settings).slideDuration,
-                        in: 0...20,
-                        onEditingChanged: { editing in
-                            isEditing = editing
+            VStack(alignment: .leading) {
+                Label("Filter by Rating", systemImage: "star.fill")
+                VStack(alignment: .leading) {
+                    HStack {
+                        Picker("Minimum: ", selection: Bindable(settings).minRating) {
+                            ForEach(0..<6) { num in
+                                Text("\(num)")
+                                    .tag(num)
+                            }
                         }
-                    ) {
-                        Text("Slide Duration: ")
+                        .pickerStyle(.segmented)
+                        .textFieldStyle(.roundedBorder)
                     }
-                    Text(String(format: "%.1f", settings.slideDuration))
-                        .foregroundColor(isEditing ? .blue : .black)
-                }
-                HStack {
-                    Slider(
-                        value: Bindable(settings).fadeDuration,
-                        in: 0...10,
-                        onEditingChanged: { editing in
-                            isEditing = editing
+                    HStack {
+                        Picker("Maximum: ", selection: Bindable(settings).maxRating) {
+                            ForEach(0..<6) { num in
+                                Text("\(num)")
+                                    .tag(num)
+                            }
                         }
-                    ) {
-                        Text("Fade Duration: ")
+                        .pickerStyle(.segmented)
+                        .textFieldStyle(.roundedBorder)
                     }
-                    Text(String(format: "%.1f", settings.fadeDuration))
-                        .foregroundColor(isEditing ? .blue : .black)
                 }
+                .padding()
+                
+                Label("Playback", systemImage: "clock")
+                VStack {
+                    HStack {
+                        Slider(
+                            value: Bindable(settings).slideDuration,
+                            in: 0...20,
+                            onEditingChanged: { editing in
+                                isEditing = editing
+                            }
+                        ) {
+                            Text("Slide Duration: ")
+                        }
+                        Text(String(format: "%.1f", settings.slideDuration))
+                            .foregroundColor(isEditing ? .blue : .black)
+                    }
+                    HStack {
+                        Slider(
+                            value: Bindable(settings).fadeDuration,
+                            in: 0...10,
+                            onEditingChanged: { editing in
+                                isEditing = editing
+                            }
+                        ) {
+                            Text("Fade Duration: ")
+                        }
+                        Text(String(format: "%.1f", settings.fadeDuration))
+                            .foregroundColor(isEditing ? .blue : .black)
+                    }
+                }
+                .padding()
             }
         }
         .padding()
